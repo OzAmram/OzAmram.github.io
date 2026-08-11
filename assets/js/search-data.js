0c6bab10f4ee5ee171c21f4e26eb005e6763795e
OzAmram <oz.amram@gmail.com>
2026-08-11 22:06:19 +0000

    Deploying to gh-pages from @ OzAmram/OzAmram.github.io@848045e51431ce55547cf9bf305e7383d7356a36 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
+        },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
         },{id: "nav-publications",
           title: "publications",
           description: "Publications grouped by research area, in reverse chronological order.",
